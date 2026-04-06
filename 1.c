185e72f018b4caedcdca28015d0b6b99621c24ac
ac212289-dev <ac212289@gmail.com>
2026-04-06 19:55:48 +0530

    hello

--- 1.c
+++ 1.c
@@ -1,6 +1,6 @@
 // this is my first c progrsmm
-   
-
-
-// gygy
-// htid is a change to my code i love merging and branching
+   #include<stdio.h>
+   int main(){
+    printf("Hello World");
+    return 0;
+   }
