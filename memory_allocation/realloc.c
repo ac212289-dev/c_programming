9a00336e1b61833d5aad0b95627b4cb1d3ef6621
ac212289-dev <ac212289@gmail.com>
2026-04-15 22:06:58 +0530

    memory allocation

---
+++ memory_allocation/realloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *arr = (int*) malloc(3 * sizeof(int));
+
+    for (int i = 0; i < 3; i++)
+        arr[i] = i + 1;
+
+    arr = (int*) realloc(arr, 6 * sizeof(int));
+
+    for (int i = 3; i < 6; i++)
+        arr[i] = i + 1;
+
+    for (int i = 0; i < 6; i++)
+        printf("%d ", arr[i]);
+
+    free(arr);
+    return 0;
+}
