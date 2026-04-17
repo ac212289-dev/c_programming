b2188c4acdb9bd849ec6198ee7dd1c21eff79753
ac212289-dev <ac212289@gmail.com>
2026-04-17 21:54:04 +0530

    .

---
+++ pointer_array/3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30;
+
+    int *ptr[3];   // array of pointers
+
+    ptr[0] = &a;
+    ptr[1] = &b;
+    ptr[2] = &c;
+
+    printf("Values using array of pointers:\n");
+
+    for(int i = 0; i < 3; i++) {
+        printf("%d ", *ptr[i]);
+    }
+
+    return 0;
+}
