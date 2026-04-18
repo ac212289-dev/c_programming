68470f2675dd9573fd1184822fc0ef25d12d74d7
ac212289-dev <ac212289@gmail.com>
2026-04-18 23:23:37 +0530

    .

---
+++ pointers/function_pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int (*ptr)(int, int);
+
+    ptr = add;
+
+    int result = ptr(2, 3);
+
+    printf("Result = %d\n", result);
+
+    return 0;
+}
