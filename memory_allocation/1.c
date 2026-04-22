4aa7ea13b63538ca3524ab826de4adb71cee1cd4
ac212289-dev <ac212289@gmail.com>
2026-04-22 23:00:27 +0530

    use of malloc

---
+++ memory_allocation/1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,n;
+    printf("enter the number of integers \n");
+    scanf("%d",&n);
+    int *ptr=(int *)malloc(n*sizeof(int));
+
+    if(ptr==NULL){
+        printf("Memory not available");
+        exit(1);
+    }
+    for(i=0;i<n;i++){
+        printf("enter an integer:");
+        scanf("%d",ptr+i);
+
+    }
+    for(i=0;i<n;i++)
+    printf("%d",*(ptr+i));
+    return 0;
+    
+}
